b2f9fcfc726b285f39eb77b2fb02dc52309c05ba
gyftang <gyftang@gmail.com>
2024-09-09 02:07:21 -0400

    adding oops

--- find_perfect_matches.sh
+++ find_perfect_matches.sh
@@ -1,7 +1,1 @@
-query="$1"
-subject="$2"
-output="$3"
-
-blastn -query "$query" -subject "$subject" -outfmt "6 pident" | awk '$1 == 100' > "$output"
-perfect_matches=$(wc -l < "$output")
-echo "$perfect_matches"
+echo "Oops!"
